90002c560ea9009ac130b80506c4d04ee204b7e3
Suk Hyun Sung <sukhsung@umich.edu>
2021-06-07 17:05:41 -0400

    readBigTiff now supports reading arbitrary pages

--- readBigTiff.m
+++ readBigTiff.m
@@ -1,28 +1,33 @@
-function im_stack = readBigTiff(fname)
+function im_stack = readBigTiff(varargin)
     
-
-    tiffObj = Tiff(fname,'r');
+    fname = varargin{1};
     im_info = imfinfo(fname);
     
     nc = im_info(1).Width;
     nr = im_info(1).Height;
     nz = length(im_info);
-   
+    
+    if nargin == 1
+        zs = 1:nz;
+    elseif nargin == 2
+        zs = varargin{2};
+        nz = length(zs);
+    else
+        error( 'Invalid Arguments' )
+    end
+    
+    tiffObj = Tiff(fname,'r');
     
     tiffObj.setDirectory(1);
     
     
     im_stack = zeros(nr,nc,nz,'single');
-    fprintf('Reading Tiff Stack \n\t%s\n',fname);
-    for z = 1:nz
-        lineLength = fprintf('%d / %d\n', z, nz);
-        im_stack(:,:,z) = tiffObj.read;
-        if ~tiffObj.lastDirectory
-            tiffObj.nextDirectory
-        end
-        fprintf(repmat('\b',1,lineLength));
+    
+    for indZ = 1:nz
+        fprintf('%d / %d\n', indZ, nz);
+        im_stack(:,:,indZ) = tiffObj.read;
+        tiffObj.setDirectory( zs(indZ))
     end
-    fprintf('Finished Reading \n\t%s\n',fname);
     close(tiffObj)
 end
         
